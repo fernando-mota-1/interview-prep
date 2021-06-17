8dd8741a237563375daefa62d3ce5a81f302d270
fernando-mota-1 <83641983+fernando-mota-1@users.noreply.github.com>
2021-06-16 21:39:43 -0400

    added iterative approach for DFS

--- graphs/discovery_DFS.py
+++ graphs/discovery_DFS.py
@@ -1,3 +1,5 @@
+from collections import deque
+
 # A class to represent a graph object
 class Graph:
     # Constructor
@@ -10,18 +12,58 @@ class Graph:
             self.adjList[src].append(dest)
             self.adjList[dest].append(src)
  
+def DFS(graph, v, discovered):
+    # create a stack used to do iterative DFS
+    stack = deque()
+ 
+    # push the source node into the stack
+    stack.append(v)
+ 
+    # loop till stack is empty
+    while stack:
+ 
+        # Pop a vertex from the stack
+        v = stack.pop()
+ 
+        # if the vertex is already discovered yet, ignore it
+        if discovered[v]:
+            continue
+ 
+        # we will reach here if the popped vertex `v`
+        # is not discovered yet; print it and process
+        # its undiscovered adjacent nodes into the stack
+        discovered[v] = True
+        print(v, end=' ')
+ 
+        # do for every edge `v —> u`
+        adj = graph.adjList[v]
+        for i in reversed(range(len(adj))):
+            u = adj[i]
+            if not discovered[u]:
+                stack.append(u)
  
 # Function to perform DFS traversal on the graph on a graph
-def DFS(graph, v, discovered):
+def recursive_DFS(graph, v, discovered):
     discovered[v] = True            # mark the current node as discovered
     print(v, end=' ')               # print the current node
  
     # do for every edge `v —> u`
     for u in graph.adjList[v]:
         if not discovered[u]:       # if `u` is not yet discovered
-            DFS(graph, u, discovered)
+            recursive_DFS(graph, u, discovered)
 
 def discover(graph, numNodes):
+    # to keep track of whether a vertex is discovered or not
+    discovered = [False] * numNodes
+ 
+    # Do iterative DFS traversal from all undiscovered nodes to
+    # cover all unconnected components of a graph
+    for i in range(numNodes):
+        if not discovered[i]:
+            DFS(graph, i, discovered)
+    print()
+
+def discover_recursive(graph, numNodes):
     # to keep track of whether a vertex is discovered or not
     discovered = [False] * N
  
@@ -31,7 +73,7 @@ def discover(graph, numNodes):
         if not discovered[i]:
             DFS(graph, i, discovered)
     print()
-    
+
 if __name__ == '__main__':
  
     # List of graph edges as per the above diagram
@@ -48,3 +90,4 @@ if __name__ == '__main__':
     graph = Graph(edges, N)
  
     discover(graph, N)
+    discover_recursive(graph, N)
